f26c44926e2a430679a7151c8b2184e92cd48ae3
hedulewei <yang_clin@aliyun.com>
2017-07-03 16:16:51 +0800

    sh

---
+++ doc/get.sh
@@ -0,0 +1,5 @@
+cp /home/jjxx_out/* /home/inspect/ftp/get/
+unzip -o  /home/inspect/ftp/get/*.zip
+chown inspect /home/inspect/ftp/get/*
+chgrp inspect /home/inspect/ftp/get/*
+cp /home/inspect/ftp/get/images/* /home/inspect/photo/
---
+++ doc/put.sh
@@ -0,0 +1,3 @@
+cp /home/inspect/ftp/put/* /home/jjxx_in/
+chown jjxx_in /home/jjxx_in/*
+chgrp jjxx_in /home/jjxx_in/*
